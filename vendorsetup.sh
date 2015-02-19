4eac55fe0ff5f3ae24654f5fc386611cc0d1952d
ashwin996 <winprabhunerurkar@gmail.com>
2015-02-19 16:55:38 -0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,6 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo aosp_nozomi-userdebug
+add_lunch_combo full_aoba-eng
+add_lunch_combo full_aoba-userdebug
+add_lunch_combo full_aoba-codefirex
