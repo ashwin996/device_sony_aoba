4dd59204d13ffa4ad96cc0a4d8b3daccb3b11f94
ashwin996 <winprabhunerurkar@gmail.com>
2015-02-19 20:09:42 -0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/sony/aoba/full_nozomi.mk)
+$(call inherit-product, device/sony/aoba/full_aoba.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := aoba
